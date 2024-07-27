8b43cbfe4f8bfb3b4bf29c7c9c43c4157302734f
zyj <89123319@qq.com>
2024-07-27 11:02:20 +0800

    添加动态加载模块

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import importlib
 
 # 获取当前工作目录
 current_working_directory = os.getcwd()
@@ -31,5 +32,24 @@ def get_MySQL_config():
 def get_log_config():
     return env_config['logging']
 
+# 获取模块列表
+def get_module_config():
+    return env_config['module_config']
+
+# 加载模块
+def load_modules(module_names='', base_path='modules'):
+    module_names = get_module_config()['modules']
+    base_path = get_module_config()['base_path']
+    loaded_modules = {}
+    for module_name in module_names:
+        try:
+            # 构造模块的完整路径
+            module_path = f"{base_path}.{module_name}"
+            module = importlib.import_module(module_path)
+            loaded_modules[module_name] = module
+        except ImportError as e:
+            print(f"Error loading module {module_name}: {e}")
+    return loaded_modules
+
 # 打印读取的配置
-# print(get_log_config())
+# print(load_modules())
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import modules.test.test as test
+import config.config as config
 
+# 在项目中通常默认是直接运行main.py文件的
 if __name__ == "__main__":
-    test.test()
+    # 加载模块
+    for module_name, module in config.load_modules().items():
+        print(f"Loaded module: {module_name}")
+        # 你可以在这里调用模块中的函数或类
+        # 例如: module.some_function()
